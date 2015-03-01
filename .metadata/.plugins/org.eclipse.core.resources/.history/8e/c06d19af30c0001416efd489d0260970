a804e6edcfaf752a6b6c1e857cde1338206a1b35
SanderSwitsers <sswitsers@gmail.com>
2015-03-01 17:34:22 +0100

    facade

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/8e/c06d19af30c0001416efd489d0260970
@@ -0,0 +1,17 @@
+package jumpingalien.part1.facade;
+
+import jumpingalien.model.Mazub;
+import jumpingalien.util.Sprite;
+
+public class Facade {
+	
+	public void createMazub(int pixelLeftX, int pixelBottomY, Sprite[] sprites){
+		
+	}
+	
+	public int[] getLocation(){
+		
+	}
+	
+
+}
--- OGP1415-JumpingAlien/src/jumpingalien/part1/facade/Facade.java
+++ OGP1415-JumpingAlien/src/jumpingalien/part1/facade/Facade.java
@@ -7,10 +7,32 @@ public class Facade {
 	
 	public void createMazub(int pixelLeftX, int pixelBottomY, Sprite[] sprites){
 		
+		Mazub myMazub = Mazub(pixelLeftX,pixelBottomY,sprites);
+		
 	}
 	
 	public int[] getLocation(){
-		
+		int[] loc={myMazub.loc_x,myMazub.loc_y};
+		return loc;
+	}
+	
+	public double[] getVelocity(){
+		double[] vel = {myMazub.vx,myMazub.vy};
+		return vel;
+	}
+	
+	public double[] getAcceleration(){
+		double[] acc = {myMazub.ax,myMazub.ay};
+		return acc;
+	}
+	
+	public int[] getSize(){
+		int[] size = {myMazub.width,myMazub.height};
+		return size;
+	} 
+	
+	public Sprite getCurrentSprite(){
+		return myMazub.sprite;
 	}
 	
 
